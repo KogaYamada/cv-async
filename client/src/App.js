eefa06e90308a90543dad39a016edaf0feaab5fc
KogaYamada <koga0087y@gmail.com>
2020-08-23 22:54:45 +0900

    cv

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  return <>App</>;
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    try {
+      const data = await axios.get('http://localhost:8000/user', {
+        headers: { apikey: 'key123' },
+      });
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      <button onClick={getData}>取得</button>
+      {data.map((user) => {
+        return (
+          <div key={user.email}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <ul>
+              {user.skills.map((skill) => {
+                return <li key={skill}>{skill}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 export default App;
