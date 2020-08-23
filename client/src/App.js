ecfc91cd0c233442ff5fff19918bb9448e94c7d3
KogaYamada <koga0087y@gmail.com>
2020-08-23 23:36:49 +0900

    保存するデータをmongodb=>jsonに変更

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,6 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    try {
-      const data = await axios.get('http://localhost:8000/user', {
-        headers: { apikey: 'key123' },
-      });
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  return (
-    <>
-      <button onClick={getData}>取得</button>
-      {data.map((user) => {
-        return (
-          <div key={user.email}>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-            <ul>
-              {user.skills.map((skill) => {
-                return <li key={skill}>{skill}</li>;
-              })}
-            </ul>
-          </div>
-        );
-      })}
-    </>
-  );
+  return <div>App Component</div>;
 };
 export default App;
--- server/server.js
+++ server/server.js
@@ -7,19 +7,20 @@ const fs = require('fs');
 const { json } = require('body-parser');
 
 const User = require('./models/user');
+const { parse } = require('path');
 
 dotenv.config();
 const app = express();
 
-const html = fs.readFileSync(`${__dirname}/index.html`, 'utf-8');
-const users = fs.readFileSync(`${__dirname}/db.json`, 'utf-8');
-
 app.use(cors());
 app.use(json());
 
-// データベース接続
+const html = fs.readFileSync(`${__dirname}/index.html`, 'utf-8');
+const usersJSON = fs.readFileSync(`${__dirname}/db.json`, 'utf-8');
+const users = JSON.parse(usersJSON);
+/* データベース接続
 mongoose
-  .connect(process.env.DATABASE || 'mongodb://localhost:27017/testapp', {
+  .connect(process.env.DATABASE , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -27,33 +28,39 @@ mongoose
   })
   .then(() => console.log('データベース接続成功'))
   .catch((error) => console.log('データベース接続失敗', error));
+*/
 
 // routes
 app.get('/', (req, res) => {
   res.send(html);
 });
 
-// app.get('/user', (req, res) => {
-//   const { apikey } = req.headers;
-//   if (apikey !== 'key123') res.status(401).send('apikeyが間違っています');
+app.get('/user', (req, res) => {
+  const { apikey } = req.headers;
+  if (apikey !== 'key123') res.status(401).send('apikeyが間違っています');
 
-//   res.json(users);
-// });
+  res.json(users);
+});
 
-// app.post('/user', async (req, res) => {
-//   const { apikey } = req.headers;
-//   if (apikey !== 'key123') res.status(401).send('apikeyが間違っています');
-//   const { name, email, skills } = req.body;
-//   try {
-//     await newUser.save();
-//   } catch (error) {
-//     console.log('ERROR', error);
-//     res.status(400).send('不正なリクエストです');
-//   } finally {
-//     res.send(newUser);
-//   }
-// });
+app.post('/user', async (req, res) => {
+  const { apikey } = req.headers;
+  if (apikey !== 'key123') res.status(401).send('apikeyが間違っています');
+  if (!req.body.name) res.status(400).send('必須項目がありません');
+  if (!req.body.email) res.status(400).send('必須項目がありません');
 
+  const reqObj = {
+    id: users.length + 1,
+    name: req.body.name,
+    email: req.body.email,
+    skills: req.body.skills ? [...req.body.skills] : [],
+  };
+  fs.writeFileSync(
+    `${__dirname}/db.json`,
+    JSON.stringify(users.concat(reqObj))
+  );
+  res.json(reqObj);
+});
+/* mongodb
 app.get('/user', async (req, res) => {
   console.log('body', req.body);
   console.log('header', req.headers);
@@ -83,7 +90,7 @@ app.post('/user', async (req, res) => {
     res.send(newUser);
   }
 });
-
+*/
 app.listen(process.env.PORT || 8000, () => {
   console.log(`サーバー起動: http://localhost:${process.env.PORT || 8000}`);
 });
